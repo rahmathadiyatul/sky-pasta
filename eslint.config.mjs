540d935b2cbd4f1bc9ee113600ef9e716ded9436
rahmathadiyatul <rahmathadiyatulakbar@gmail.com>
2025-03-12 18:45:16 +0700

    Eslint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element": "off",
     },
   },
 ];
